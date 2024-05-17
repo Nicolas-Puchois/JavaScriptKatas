fc95764fc32788a461d51b6a199da6360a3c9c9a
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-17 11:31:13 +0200

    feat(exercice-katas-1.js): add file

---
+++ exercice-katas-1.js
@@ -0,0 +1,32 @@
+/**
+ Looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3 
+ */
+
+'use strict';
+
+let triangle = "#"; 
+
+console.log(triangle);
+
+for(let conditiontriangle = 0; conditiontriangle<8; conditiontriangle++){
+    
+    triangle += "#";
+    console.log(triangle)
+ //   console.log(triangle.length); --> permet de savoir la longueur de la chaines de caractère
+};

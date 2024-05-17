5239510b5d6e5229b36b9c7a143a20185210a07e
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-17 15:31:08 +0200

    feat(exercice-katas-4.js): add exercice answer

---
+++ exercice-katas-4.js
@@ -0,0 +1,19 @@
+/*Minimum
+
+Write a function min that takes two arguments and returns their minimum.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+*/
+
+let firstnumber = +prompt("Entrer votre premier nombre",1);
+let secondnumber  = +prompt("Entrer votre second nombre",2);
+
+let resultat = (firstnumber > secondnumber) ? secondnumber : firstnumber;
+
+
+
+console.log(resultat);
